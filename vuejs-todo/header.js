66f50e1fc745d2e2d73b85835764e430c6ce3460
Your Name <you@example.com>
2020-03-09 14:31:51 -0300

    projetos

---
+++ vuejs-todo/header.js
@@ -0,0 +1,8 @@
+Vue.component('my-head', { 
+    template: `<div class="header"></div>`,
+    style: `.header: {
+        width: 200px;
+        height: 200px;
+        background-color: blue;
+    }`
+ })
---
+++ vuejs-todo/main.js
@@ -0,0 +1,19 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        todos: [{
+            title: "todo1",
+            finished: false
+        }, {
+            title: "todo2",
+            finished: false
+        }, {
+            title: "todo3",
+            finished: false
+        }]
+    }, methods: {
+
+    }, components: {
+        
+    }
+})
